dc81d64528bd2c3e0c2d24bf8b427f30521ff54c
YAHIAG13 <78378644+YAHIAG13@users.noreply.github.com>
2023-04-03 15:49:14 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ URL::asset('style/welcome.css')}}">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div id="header">
+            <div id="logo">
+                Brand Name
+            </div>
+            <div id="menu">
+                <ul>
+                    <li>
+                        <a href="#">Products</a>
+                    </li>
+                    <li>
+                        <a href="#">Products</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div id="search">
+
+            </div>
+            <div id="account">
+                <p>Account</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

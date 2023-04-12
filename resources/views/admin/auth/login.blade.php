87749f27c4f736e52609d686b103cd1c9cd5605d
YAHIAG13 <78378644+YAHIAG13@users.noreply.github.com>
2023-04-12 15:17:44 +0100

    styled the login for admins

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -4,23 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ URL::asset('style/adm-login.css')}}">
     <title>Control Panel: Login</title>
 </head>
 <body>
-    <form action="{{ route('login-c') }}" method="post">
-        @csrf
-        <label for="name">Name:</label><br>
-        <input type="text" name="name" id="name"><br>
-        @if ($errors->has('name'))
-            <span class="text-danger">{{ $errors->first('name') }}</span>
-        @endif
-
-        <label for="password">Password:</label><br>
-        <input type="password" name="password" id="password">
-        @if ($errors->has('password'))
-            <span class="text-danger">{{ $errors->first('password') }}</span>
-        @endif
-        <input type="submit" value="Log In">
-    </form>
+    <h1>Control Panel</h1>
+    <div class="container">
+        <form action="{{ route('login-c') }}" method="post">
+            @csrf
+            <label for="name">Name:</label><br>
+            <input class="text" type="text" name="name" id="name" placeholder="your name..."><br>
+            @if ($errors->has('name'))
+                <span class="error">{{ $errors->first('name') }}</span>
+            @endif
+            <br>
+            <label for="password">Password:</label><br>
+            <input class="text" type="password" name="password" id="password" placeholder="your password..."><br>
+            @if ($errors->has('password'))
+                <span class="error">{{ $errors->first('password') }}</span>
+            @endif
+            <br><br>
+            <div class="submit">
+                <input type="submit" value="Log In">
+            </div>
+        </form>
+    </div>
 </body>
 </html>

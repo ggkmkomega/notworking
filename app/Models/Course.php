2d50b5e8d34fd15312f9bf07c5ccba76da403ba9
YAHIAG13 <78378644+YAHIAG13@users.noreply.github.com>
2023-05-11 17:13:10 +0100

    some frontend

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use \Awobaz\Compoships\Compoships;
 
 class Course extends Product
